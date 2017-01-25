edab79d4241f82104a22d6fb5bad8de5ef3c8713
SpencerMorrill <spencermorrill@gmail.com>
2017-01-25 13:33:38 -0500

    Create module1.js

---
+++ code/module1.js
@@ -0,0 +1,13 @@
+# CODE EXAMPLES FROM CODE SCHOOL
+
+#Make 18 with +, -, /, or *
+36/2
+
+#Make 38 with +, -, /, or *
+23+15
+
+#Make 100 with +, -, /, or *
+10*10
+
+#Make 51 with +, -, /, or *
+71-20
